0b11fb47de9cd931e308dc959eac9a9870ec76ad
Trustcode inc <ping.host.adm@gmail.com>
2018-08-23 23:33:38 +0300

    Last commit'

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@
  Функция должна добавлять обработчик fn события eventName к элементу target
 
  Пример:
-   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
+   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить 
+   указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn);
 }
 
 /*
@@ -17,20 +19,27 @@ function addListener(eventName, target, fn) {
  Функция должна удалять у элемента target обработчик fn события eventName
 
  Пример:
-   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
+   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный 
+   обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+    target.removeEventListener(eventName, fn);
 }
 
 /*
  Задание 3:
 
- Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял действия по умолчанию
+ Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял 
+ действия по умолчанию
 
  Пример:
-   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
+   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную 
+   ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+    target.addEventListener(eventName, function (eventName) {
+        eventName.preventDefault();
+    });
 }
 
 /*
@@ -39,9 +48,11 @@ function skipDefault(eventName, target) {
  Функция должна эмулировать событие click для элемента target
 
  Пример:
-   emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
+   emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано 
+   события click
  */
 function emulateClick(target) {
+    target.click();
 }
 
 /*
@@ -51,21 +62,36 @@ function emulateClick(target) {
  который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
 
  Пример:
-   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
+   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик 
+   кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку 
+   (элемент с тегом button)
  */
 function delegate(target, fn) {
+    target.addEventListener('click', function (e) {
+        if (e.target.tagName == 'BUTTON') {
+            fn();
+        }
+    })
 }
 
 /*
  Задание 6:
 
  Функция должна добавить такой обработчик кликов к элементу target,
- который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по указанному элементу)
+ который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по 
+ указанному элементу)
 
  Пример:
-   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
+   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит 
+   такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+    const newFunc = () => {
+        fn();
+        target.removeEventListener('click', newFunc);
+    };
+
+    target.addEventListener('click', newFunc);
 }
 
 export {
